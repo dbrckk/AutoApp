d5eb7f39eada31daacf491f7985d3bb07191a8bc
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-05-22 20:17:55 +0200

    Create react-syntax-highlighter.d.ts

---
+++ src/types/react-syntax-highlighter.d.ts
@@ -0,0 +1,59 @@
+declare module "react-syntax-highlighter/dist/esm/prism-light" {
+  const PrismLight: any;
+  export default PrismLight;
+}
+
+declare module "react-syntax-highlighter/dist/esm/styles/prism/vsc-dark-plus" {
+  const theme: any;
+  export default theme;
+}
+
+declare module "react-syntax-highlighter/dist/esm/languages/prism/typescript" {
+  const language: any;
+  export default language;
+}
+
+declare module "react-syntax-highlighter/dist/esm/languages/prism/tsx" {
+  const language: any;
+  export default language;
+}
+
+declare module "react-syntax-highlighter/dist/esm/languages/prism/javascript" {
+  const language: any;
+  export default language;
+}
+
+declare module "react-syntax-highlighter/dist/esm/languages/prism/jsx" {
+  const language: any;
+  export default language;
+}
+
+declare module "react-syntax-highlighter/dist/esm/languages/prism/css" {
+  const language: any;
+  export default language;
+}
+
+declare module "react-syntax-highlighter/dist/esm/languages/prism/json" {
+  const language: any;
+  export default language;
+}
+
+declare module "react-syntax-highlighter/dist/esm/languages/prism/markup" {
+  const language: any;
+  export default language;
+}
+
+declare module "react-syntax-highlighter/dist/esm/languages/prism/markdown" {
+  const language: any;
+  export default language;
+}
+
+declare module "react-syntax-highlighter/dist/esm/languages/prism/yaml" {
+  const language: any;
+  export default language;
+}
+
+declare module "react-syntax-highlighter/dist/esm/languages/prism/bash" {
+  const language: any;
+  export default language;
+}
